cd2ba66a41fb45895f6315300a0194897394a7cb
lylodlig <253686100@qq.com>
2018-05-05 22:57:00 +0800

    first commit

---
+++ app/src/main/java/com/lzy/testproject/constraintlayout/TestActivity.java
@@ -0,0 +1,15 @@
+package com.lzy.testproject.constraintlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lzy.testproject.R;
+
+public class TestActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test2);
+	}
+}
---
+++ app/src/main/java/com/lzy/testproject/searchview/TestActivity.java
@@ -0,0 +1,41 @@
+package com.lzy.testproject.searchview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lzy.testproject.R;
+
+public class TestActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+
+		SearchView searchView = findViewById(R.id.searchView);
+		TextView textView = searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+		textView.setHint("东方航空是");
+		textView.setTextSize(16);
+//		searchView.setIconified(false);
+		searchView.setIconifiedByDefault(false);
+//		searchView.onActionViewExpanded();
+		searchView.setQueryHint("sdfsfs");
+
+//		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//		imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+
+		final View view = findViewById(R.id.view);
+
+		view.post(new Runnable() {
+			@Override
+			public void run() {
+				view.requestFocus();
+
+			}
+		});
+
+	}
+}
