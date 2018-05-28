d71f81858e6a319177af535c4b4694706489144b
李枝宇 <lizhiyu@dgg.net>
2018-05-28 17:29:59 +0800

    更新2

--- app/src/main/java/com/lzy/testproject/rx/RxActivity.java
+++ app/src/main/java/com/lzy/testproject/rx/RxActivity.java
@@ -6,11 +6,18 @@ import android.util.Log;
 
 import com.lzy.testproject.R;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxActivity extends AppCompatActivity {
 
@@ -21,7 +28,12 @@ public class RxActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx);
 
-        testCreate();
+//        testCreate();
+//        testJust();
+//        testRange();
+//        intervalRange();
+        countDown();
+
     }
 
     private void testCreate() {
@@ -57,4 +69,78 @@ public class RxActivity extends AppCompatActivity {
             }
         });
     }
+
+    //Consume只关心next事件
+    private void testJust() {
+        Observable.just("Love", "For", "You!")
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.i(TAG, "accept: " + s);
+                    }
+                });
+    }
+
+    //start:起始值；count：数量
+    //Consume只关心next事件
+    private void testRange() {
+        Observable.range(5, 3)
+                //输出 5,6,7
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.i(TAG, "accept: " + integer);
+                    }
+                });
+    }
+
+    /**
+     * start,count:同range
+     * initialDelay 发送第一个值的延迟时间
+     * period 每两个发射物的间隔时间
+     * unit,scheduler 额你懂的
+     */
+    private void intervalRange() {
+        Observable.intervalRange(5, 3, 1, 2, TimeUnit.SECONDS, Schedulers.io())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        Log.i(TAG, "accept: " + aLong);
+                    }
+                });
+    }
+
+    private void countDown() {
+        final long time = 10;
+        Observable.interval(1, TimeUnit.SECONDS)
+                .map(new Function<Long, Long>() {
+                    @Override
+                    public Long apply(Long aLong) throws Exception {
+                        return time - aLong;
+                    }
+                }).take(time + 1)//发生time+1前条数据
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Exception {
+                        Log.i(TAG, "accept: 开始之前");
+                    }
+                }).subscribeWith(new DisposableObserver<Long>() {
+            @Override
+            public void onNext(Long value) {
+                Log.i(TAG, "onNext: " + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete: ");
+            }
+        });
+    }
+
+
 }
