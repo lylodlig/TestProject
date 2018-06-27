53db83366ec685b9a4b102bf2e174a499463b3ac
lylodlig <253686100@qq.com>
2018-06-28 00:57:30 +0800

    kotlin

---
+++ app/src/main/java/com/lzy/testproject/nestedscrolling/MyNestedScrollParent.java
@@ -0,0 +1,98 @@
+package com.lzy.testproject.nestedscrolling;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.NestedScrollingParent;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Scroller;
+
+public class MyNestedScrollParent extends LinearLayout implements NestedScrollingParent {
+
+
+    private int mTopViewHeight=400;
+    private Scroller mScroller;
+
+    public MyNestedScrollParent(Context context) {
+        super(context);
+        mScroller = new Scroller(context);
+    }
+
+    public MyNestedScrollParent(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        mScroller = new Scroller(context);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes) {
+        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;//如果是纵向滑动返回true
+    }
+
+    @Override
+    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes) {
+
+    }
+
+    @Override
+    public void onStopNestedScroll(@NonNull View target) {
+
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+
+    }
+
+    //嵌套滑动时调用
+    @Override
+    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed) {
+        boolean hiddenTop = dy > 0 && getScrollY() < mTopViewHeight;//向上滑动，隐藏header
+        boolean showTop = dy < 0 && getScrollY() > 0 && !ViewCompat.canScrollVertically(target, -1);//向下显示
+
+        if (hiddenTop || showTop) {
+            scrollBy(0, dy);
+            consumed[1] = dy;
+        }
+    }
+
+    @Override
+    public boolean onNestedFling(@NonNull View target, float velocityX, float velocityY, boolean consumed) {
+        return false;
+    }
+
+    @Override
+    public boolean onNestedPreFling(@NonNull View target, float velocityX, float velocityY) {
+        if (getScrollY() >= mTopViewHeight) return false;
+        fling((int) velocityY);
+        return true;
+    }
+    public void fling(int velocityY)
+    {
+        mScroller.fling(0, getScrollY(), 0, velocityY, 0, 0, 0, mTopViewHeight);
+        invalidate();
+    }
+
+    @Override
+    public void scrollTo(int x, int y)
+    {
+        if (y < 0)
+        {
+            y = 0;
+        }
+        if (y > mTopViewHeight)
+        {
+            y = mTopViewHeight;
+        }
+        if (y != getScrollY())
+        {
+            super.scrollTo(x, y);
+        }
+    }
+    @Override
+    public int getNestedScrollAxes() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/lzy/testproject/nestedscrolling/NestedScrollingActivity.java
@@ -0,0 +1,15 @@
+package com.lzy.testproject.nestedscrolling;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.lzy.testproject.R;
+
+public class NestedScrollingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nested_scrolling);
+    }
+}
