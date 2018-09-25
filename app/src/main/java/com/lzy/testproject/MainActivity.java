8780e0b2b61e9057d4b2dd7004044a9068744307
lylodlig <253686100@qq.com>
2018-09-26 00:53:34 +0800

    提交

--- app/src/main/java/com/lzy/testproject/MainActivity.java
+++ app/src/main/java/com/lzy/testproject/MainActivity.java
@@ -2,14 +2,38 @@ package com.lzy.testproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+		List<String> list=new ArrayList<>();
+		for (int i = 0; i < list.size(); i++) {
+			list.add("i" + i);
+		}
+
+		String s = "3";
 
 
-    }
+		if (list != null) {
+			for (String ignored : list) {
+				s = ignored;
+			}
+		}
+
+		s = "10";
+
+	}
+
+	private void test(){
+
+	}
 }
--- app/src/main/java/com/lzy/testproject/ui/recyclerview/diffcallback/DiffActivity.kt
+++ app/src/main/java/com/lzy/testproject/ui/recyclerview/diffcallback/DiffActivity.kt
@@ -1,7 +1,7 @@
 package com.lzy.testproject.ui.recyclerview.diffcallback
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
