8d533f0023fad218add9d1fe1b238650fd5623cd
李枝宇 <lizhiyu@dgg.net>
2018-10-30 10:59:18 +0800

    更新

--- app/src/main/java/com/lzy/testproject/MyApplication.java
+++ app/src/main/java/com/lzy/testproject/MyApplication.java
@@ -1,8 +1,15 @@
 package com.lzy.testproject;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+
+import org.jetbrains.annotations.NotNull;
 
 import tech.linjiang.pandora.Pandora;
 
@@ -19,5 +26,76 @@ public class MyApplication extends Application {
 //        ARouter.openLog();     // 打印日志
 //        ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
 //        ARouter.init( this); // 尽可能早，推荐在Application中初始化
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                setCustomDensity(activity, MyApplication.this);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+
+    private static float sNoncompatDensity;
+    private static float sNoncompatScaleDensity;
+
+    private void setCustomDensity(@NotNull Activity activity, @NotNull final Application application) {
+        final DisplayMetrics appDisplayMetrics = application.getResources().getDisplayMetrics();
+        if (sNoncompatDensity == 0) {
+            sNoncompatDensity = appDisplayMetrics.density;
+            sNoncompatScaleDensity = appDisplayMetrics.scaledDensity;
+            application.registerComponentCallbacks(new ComponentCallbacks() {
+                @Override
+                public void onConfigurationChanged(Configuration newConfig) {
+                    if (newConfig != null && newConfig.fontScale > 0) {
+                        sNoncompatScaleDensity = application.getResources().getDisplayMetrics().scaledDensity;
+                    }
+                }
+
+                @Override
+                public void onLowMemory() {
+                }
+            });
+        }
+        final float targetDensity = appDisplayMetrics.widthPixels / 360;
+        final float targetScaleDensity = targetDensity * (sNoncompatScaleDensity / sNoncompatDensity);
+        final int targetDensityDpi = (int) (targetDensity * 160);
+
+        appDisplayMetrics.density = targetDensity;
+        appDisplayMetrics.scaledDensity = targetScaleDensity;
+        appDisplayMetrics.densityDpi = targetDensityDpi;
+
+        final DisplayMetrics displayMetrics = activity.getResources().getDisplayMetrics();
+        displayMetrics.densityDpi = targetDensityDpi;
+        displayMetrics.density = targetDensity;
+        displayMetrics.scaledDensity = targetScaleDensity;
     }
 }
