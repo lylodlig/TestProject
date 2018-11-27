40a8cfca706cbe51d0fe83777ecb643fc830e946
李枝宇 <lizhiyu@dgg.net>
2018-11-27 15:33:22 +0800

    3

--- app/src/main/java/com/lzy/testproject/test/TestActivity.java
+++ app/src/main/java/com/lzy/testproject/test/TestActivity.java
@@ -41,6 +41,7 @@ public class TestActivity extends AppCompatActivity {
         float val = c / 10000;
         Log.i("lzy", "bt: 哈哈");
         Log.i("lzy", "bt: 哈哈");
+        Log.i("lzy", "bt: 哈哈");
         String format = new DecimalFormat("#.0").format(val);
         BigDecimal bigDecimal = new BigDecimal(val);
         bigDecimal.setScale(1, BigDecimal.ROUND_HALF_UP);
