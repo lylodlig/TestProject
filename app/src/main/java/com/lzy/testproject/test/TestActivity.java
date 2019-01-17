917dca3c92bb0204a80b65c45fc0fe93ec2049d8
李枝宇 <lizhiyu@dgg.net>
2019-01-17 11:50:55 +0800

    update

--- app/src/main/java/com/lzy/testproject/test/TestActivity.java
+++ app/src/main/java/com/lzy/testproject/test/TestActivity.java
@@ -2,18 +2,25 @@ package com.lzy.testproject.test;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.blankj.utilcode.util.DeviceUtils;
 import com.leeiidesu.permission.PermissionHelper;
@@ -22,8 +29,13 @@ import com.lzy.testproject.R;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -35,10 +47,19 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test3);
-        Log.i("lzy", "onCreate: " + DeviceUtils.getManufacturer());
 
-        printTelephonyManagerMethodNamesForThisDevice();
-        getSubscriberId();
+        HashSet set = new HashSet();
+        set.add("df");
+        set.add(null);
+        set.add("df");
+        set.add("d1");
+        Log.i("lzy", "onCreate: ");
+//        printTelephonyManagerMethodNamesForThisDevice();
+//        getSubscriberId();
+
+        Calendar calendar = Calendar.getInstance();
+        int i = calendar.get(Calendar.MONTH);
+        int j = calendar.get(Calendar.DAY_OF_MONTH);
 
         mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         if (mTelephonyManager == null) {
@@ -46,21 +67,77 @@ public class TestActivity extends AppCompatActivity {
         }
 //        mConnMngr = (ConnectivityManager).getSystemService(Context.CONNECTIVITY_SERVICE);
         mSubscriptionManager = SubscriptionManager.from(this);
+//        getPhoneNumber();
+    }
 
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void sendSms(final int which) {
+        SubscriptionInfo sInfo = null;
+        final SubscriptionManager sManager = (SubscriptionManager) getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        List<SubscriptionInfo> list = sManager.getActiveSubscriptionInfoList();
+
+
+        if (list.size() == 2) {// double card
+            sInfo = list.get(which);
+        } else {//single card
+            sInfo = list.get(0);
+        }
+        String number = sInfo.getNumber();
+        if (sInfo != null) {
+//                    String provider = matchIMSI(String.valueOf(sInfo.getMcc()) + "0"
+//                    + String.valueOf(sInfo.getMnc()));
+            int subId = sInfo.getSubscriptionId();
+//            Log.i("lzy", " select provider = " + provider + ", subid = "
+//                    + subId);
+            new Thread() {
+                @Override
+                public void run() {
+                    super.run();
+                    SmsManager manager = SmsManager
+                            .getSmsManagerForSubscriptionId(subId);
+                    manager.sendTextMessage("10010", null, "YE", null, null);
+                }
+            }.start();
+
+            Toast.makeText(this, "信息正在发送，请稍候", Toast.LENGTH_SHORT)
+                    .show();
+//            if (!TextUtils.isEmpty(provider)) {
+//                manager.sendTextMessage("17628291537", null, "YE", null, null);
+//                Toast.makeText(this, "信息正在发送，请稍候", Toast.LENGTH_SHORT)
+//                        .show();
+//            } else {
+//                Toast.makeText(this, "无法正确的获取SIM卡信息，请稍候重试",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+        }
     }
 
 
     public void bt(View view) {
 //        getPhoneNumber();
-        String msisdn = getMsisdn(0);
-        String msisdn1 = getMsisdn(1);
-        getLine1NumberForSubscriber(0);
-        getLine1NumberForSubscriber(1);
+//        String msisdn = getMsisdn(0);
+//        String msisdn1 = getMsisdn(1);
+//        getLine1NumberForSubscriber(0);
+//        getLine1NumberForSubscriber(1);
+        sendSms(1);
+        sendSms(0);
     }
 
     private String getPhoneNumber() {
         PermissionHelper.with(this)
-                .permissions(Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_PHONE_NUMBERS)
+                .permissions(Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_PHONE_NUMBERS, Manifest.permission.SEND_SMS)
                 .listener(new OnPermissionResultListener() {
                     @SuppressLint("MissingPermission")
                     @Override
@@ -175,13 +252,7 @@ public class TestActivity extends AppCompatActivity {
             method = mTelephonyManager.getClass().getMethod("getLine1Number", int.class);
             method.setAccessible(true);
             status = String.valueOf(method.invoke(mTelephonyManager, subId));
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         Log.i("lzy", "dff " + status);
